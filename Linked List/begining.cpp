b92eb98a2ab53155cecce43807a2ee68e1d8465b
Jaskirat-29 <jaskiratsinghbindra29@gmail.com>
2025-09-23 12:43:20 +0530

    begining

---
+++ Linked List/begining.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+struct node{
+    int info;
+    struct node*next;
+};
+struct node*start=NULL, *new_node;
+struct node *create_node(int item){
+    new_node=(struct node*)malloc(sizeof(struct node));
+    new_node->info=item;
+    new_node->next=NULL;
+    return new_node;
+
+};
+void insert(){
+    struct node *ptr;
+    int item;
+    cout<<"enter the item:";
+    cin>>item;
+    ptr=create_node(item);
+if(ptr==NULL){
+    cout<<"failed to allocate the memory";
+
+}
+else if(start==NULL){
+    start=ptr;
+}
+else{
+    ptr->next=start;
+    start=ptr;
+}
+}
+void display(){
+    if(start==NULL){
+        cout<<"LL is empty"<<endl;
+    }
+    else{
+        struct node*temp;
+        temp=start;
+        while(temp!=NULL){
+            cout<<temp->info<<endl;
+            temp=temp->next;
+
+        }
+
+    }
+}
+ int main(){
+    int choice=0;
+   
+    while(choice!=3){
+       
+        cout<<"1.Press 1 to insert item\n,2. Press 2 to display\n,3. Press 3 to exit\n";
+         cin>>choice;
+        switch(choice){
+            case 1:
+            insert();
+            break;
+            case 2:
+            display();
+            break;
+            case 3:
+            exit(0);
+            break;
+            default:
+            cout<<"Invalid choice";
+            break;
+        }
+}
+}
+
+
