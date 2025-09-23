4d7989516b3eb05ea1b452057d1cface9769d8c9
Jaskirat-29 <jaskiratsinghbindra29@gmail.com>
2025-09-24 01:24:37 +0530

    end

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,71 +1,101 @@
 #include<iostream>
-#include<stdlib.h>
+#include <stdlib.h>
+
 using namespace std;
-struct node{
+
+struct node
+{
     int info;
-    struct node*next;
+    struct node *next;
 };
-struct node*start=NULL, *new_node;
-struct node *create_node(int item){
-    new_node=(struct node*)malloc(sizeof(struct node));
+struct node *start;
+
+struct node *createnewnode(int item)
+{
+    struct node *new_node=(struct node*)malloc(sizeof(struct node));
     new_node->info=item;
     new_node->next=NULL;
     return new_node;
-
 };
+
 void insert(){
     struct node *ptr;
     int item;
-    cout<<"enter the item:";
+    cout<<"Enter item value:";
     cin>>item;
-    ptr=create_node(item);
-if(ptr==NULL){
-    cout<<"failed to allocate the memory";
+    ptr= createnewnode(item);
+    if (ptr==NULL){
+        cout<<"Failed to allocate memory";
+    }
+    else if(start==NULL){
+        start=ptr;
+        cout<<"Item inserted\n";
+    }
+    else {
+        ptr->next=start;
+        start=ptr;
+        cout<<"Item inserted\n";
+    }
+}
 
-}
-else if(start==NULL){
-    start=ptr;
-}
-else{
-    ptr->next=start;
-    start=ptr;
-}
-}
 void display(){
-    if(start==NULL){
-        cout<<"LL is empty"<<endl;
+    if (start==NULL){
+        cout<<"Linked list is empry\n";
     }
-    else{
-        struct node*temp;
+    else {
+        struct node *temp;
         temp=start;
-        while(temp!=NULL){
-            cout<<temp->info<<endl;
+        while (temp != NULL)
+        {
+            cout<<"The element is:"<<temp->info<<"\n";
             temp=temp->next;
-
         }
-
+        
     }
 }
- int main(){
+void insert_end(){
+    struct node *new_node=(struct node*)malloc(sizeof(struct node));
+    int item;
+    cout<<"Enter value to insert at the end:"<<"\n";
+    cin>>item;
+    new_node->info=item;
+    new_node->next=NULL;
+    struct node *temp;
+    temp=start;
+    while (temp->next != NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=new_node;
+    
+}
+main() {
     int choice=0;
-   
-    while(choice!=3){
-       
-        cout<<"1.Press 1 to insert item\n,2. Press 2 to display\n,3. Press 3 to exit\n";
-         cin>>choice;
-        switch(choice){
-            case 1:
+    while (choice!=3)
+    {
+        cout<<"Press 1. To insert item at beginning\n 2.Press 2. To Display\n 3.To exit";
+        cin>>choice;
+        switch (choice)
+        {
+        case 1:
             insert();
             break;
-            case 2:
+        case 2:
             display();
             break;
-            case 3:
-            exit(0);
-            break;
-            default:
-            cout<<"Invalid choice";
+        default:
+            cout<<"Wrong Input\n";
             break;
         }
-}
+    }
+    int perform;
+    cout<<"Press 1 to insert element at the end\n";
+    cin>>perform;
+    if (perform==1){
+        insert_end();
+    }
+    else {
+        cout<<"Wrong input";
+    }
+    display();
 }
