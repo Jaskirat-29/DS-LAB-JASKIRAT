fef4375d61621b9bf4c27db1ac7a908c0eea1e50
Jaskirat-29 <jaskiratsinghbindra29@gmail.com>
2025-09-25 15:02:45 +0530

    Middle

--- Linked List/Middle.cpp
+++ Linked List/Middle.cpp
@@ -8,94 +8,112 @@ struct node
     int info;
     struct node *next;
 };
-struct node *start;
+struct node *start = NULL;
 
 struct node *createnewnode(int item)
 {
-    struct node *new_node=(struct node*)malloc(sizeof(struct node));
-    new_node->info=item;
-    new_node->next=NULL;
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->info = item;
+    new_node->next = NULL;
     return new_node;
-};
+}
 
 void insert(){
     struct node *ptr;
     int item;
-    cout<<"Enter item value:";
-    cin>>item;
-    ptr= createnewnode(item);
-    if (ptr==NULL){
-        cout<<"Failed to allocate memory";
+    cout << "Enter item value: ";
+    cin >> item;
+    ptr = createnewnode(item);
+    if (ptr == NULL){
+        cout << "Failed to allocate memory\n";
     }
-    else if(start==NULL){
-        start=ptr;
-        cout<<"Item inserted\n";
+    else if(start == NULL){
+        start = ptr;
+        cout << "Item inserted\n";
     }
     else {
-        ptr->next=start;
-        start=ptr;
-        cout<<"Item inserted\n";
+        ptr->next = start;
+        start = ptr;
+        cout << "Item inserted\n";
     }
 }
 
 void display(){
-    if (start==NULL){
-        cout<<"Linked list is empry\n";
+    if (start == NULL){
+        cout << "Linked list is empty\n";
     }
     else {
-        struct node *temp;
-        temp=start;
+        struct node *temp = start;
         while (temp != NULL)
         {
-            cout<<"The element is:"<<temp->info<<"\n";
-            temp=temp->next;
+            cout << "The element is: " << temp->info << "\n";
+            temp = temp->next;
         }
-        
     }
 }
-void insert_end(){
-    struct node *new_node=(struct node*)malloc(sizeof(struct node));
-    int item;
-    cout<<"Enter value to insert at the end:"<<"\n";
-    cin>>item;
-    new_node->info=item;
-    new_node->next=NULL;
-    struct node *temp;
-    temp=start;
-    while (temp->next != NULL)
-    {
-        temp=temp->next;
+
+void insert_pos(){
+    int item, pos;
+    cout << "Enter value to insert: ";
+    cin >> item;
+    cout << "Enter the position to insert at: ";
+    cin >> pos;
+
+    struct node *new_node = createnewnode(item);
+
+    if(pos == 1){
+        new_node->next = start;
+        start = new_node;
+        return;
     }
-    temp->next=new_node;
-    
+
+    struct node *temp = start;
+    for(int i = 1; i < pos - 1 && temp != NULL; i++){
+        temp = temp->next;
+    }
+
+    if(temp == NULL){
+        cout << "Position out of bounds\n";
+        free(new_node);
+        return;
+    }
+
+    new_node->next = temp->next;
+    temp->next = new_node;
+    cout << "Item inserted at position " << pos << "\n";
 }
-main() {
-    int choice=0;
-    while (choice!=3)
+
+int main() {
+    int choice = 0;
+    while (choice != 3)
     {
-        cout<<"Press 1. To insert item at beginning\n 2.Press 2. To Display\n 3.To exit";
-        cin>>choice;
+        cout << "Press 1. To insert item at beginning\n2. To Display\n3. To exit\n";
+        cin >> choice;
         switch (choice)
         {
-        case 1:
-            insert();
-            break;
-        case 2:
-            display();
-            break;
-        default:
-            cout<<"Wrong Input\n";
-            break;
+            case 1:
+                insert();
+                break;
+            case 2:
+                display();
+                break;
+            case 3:
+                break;
+            default:
+                cout << "Wrong Input\n";
         }
     }
+
     int perform;
-    cout<<"Press 1 to insert element at the end\n";
-    cin>>perform;
-    if (perform==1){
-        insert_end();
+    cout << "Press 1 to insert element at a specific position: ";
+    cin >> perform;
+    if (perform == 1){
+        insert_pos();
+        display();
     }
     else {
-        cout<<"Wrong input";
+        cout << "Wrong input\n";
     }
-    display();
+
+    return 0;
 }
